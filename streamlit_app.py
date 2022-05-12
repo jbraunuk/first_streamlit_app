393194beb90272ceb7b49009b6fb0d6dbaa7270c
jbraunuk <85951123+jbraunuk@users.noreply.github.com>
2022-05-12 20:18:57 +0100

    Update streamlit_app.py
    Filter results to selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,10 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit') 
 
 # create a pick list
-streamlit.multiselect("Pick some fruita:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruita:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc['fruits_selected']
 
 #display the dataframe
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
 
 
